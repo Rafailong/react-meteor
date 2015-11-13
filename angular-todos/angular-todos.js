f5d80e83375c85d7a07ae39e8d154e47f6a3efd5
Rafael Avila <rafa.avim@gmail.com>
2015-11-13 19:28:06 +0000

    complete angular todos with meteor

--- angular-todos/angular-todos.js
+++ angular-todos/angular-todos.js
@@ -1,23 +1,101 @@
+Todos = new Mongo.Collection('todos');
+
 if (Meteor.isClient) {
-  // counter starts at 0
-  Session.setDefault('counter', 0);
-
-  Template.hello.helpers({
-    counter: function () {
-      return Session.get('counter');
-    }
+  
+  Accounts.ui.config({
+    passwordSignupFields: "USERNAME_ONLY"
   });
+  
+  angular.module('simple-todos', ['angular-meteor', 'accounts.ui']);
+  
+  angular.module('simple-todos').controller('TodosListCtrl', 
+    ['$scope', '$meteor', function ($scope, $meteor) {
+      
+    $scope.$meteorSubscribe('todos');
+    
+    $scope.todos = $meteor.collection(function () {
+      return Todos.find($scope.getReactively('query'), { sort: { createdAt: -1 } });
+    });
+    
+    $scope.addTask = function (newTask) {
+      $meteor.call('addTask', newTask);
+    };
 
-  Template.hello.events({
-    'click button': function () {
-      // increment the counter when button is clicked
-      Session.set('counter', Session.get('counter') + 1);
+    $scope.deleteTask = function (task) {
+      $meteor.call('deleteTask', task._id);
+    };
+
+    $scope.setChecked = function (task) {
+      $meteor.call('setChecked', task._id, !task.checked);
+    };
+    
+    $scope.setPrivate = function (task) {
+      $meteor.call('setPrivate', task._id, !task.private);
     }
-  });
+    
+    $scope.$watch('hideCompleted', function() {
+      if ($scope.hideCompleted)
+        $scope.query = {checked: {$ne: true}};
+      else
+        $scope.query = {};
+    });
+    
+    $scope.incompleteCount = function () {
+      return Todos.find({ checked: {$ne: true} }).count();
+    };
+  }]);
 }
 
+Meteor.methods({
+  addTask: function (text) {
+    // Make sure the user is logged in before inserting a task
+    if (! Meteor.userId()) {
+      throw new Meteor.Error('not-authorized');
+    }
+ 
+    Todos.insert({
+      text: text,
+      createdAt: new Date(),
+      owner: Meteor.userId(),
+      username: Meteor.user().username
+    });
+  },
+  deleteTask: function (taskId) {
+    var task = Tasks.findOne(taskId);
+    if (task.private && task.owner !== Meteor.userId()) {
+      // If the task is private, make sure only the owner can check it off
+      throw new Meteor.Error('not-authorized');
+    }
+    
+    Todos.remove(taskId);
+  },
+  setChecked: function (taskId, setChecked) {
+    var task = Tasks.findOne(taskId);
+    if (task.private && task.owner !== Meteor.userId()) {
+      // If the task is private, make sure only the owner can check it off
+      throw new Meteor.Error('not-authorized');
+    }
+    
+    Todos.update(taskId, { $set: { checked: setChecked} });
+  },
+  setPrivate: function (taskId, setToPrivate) {
+    var task = Tasks.findOne(taskId);
+    if (task.private && task.owner !== Meteor.userId()) {
+      // If the task is private, make sure only the owner can check it off
+      throw new Meteor.Error('not-authorized');
+    }
+ 
+    Todos.update(taskId, { $set: { private: setToPrivate } });
+  }
+});
+
 if (Meteor.isServer) {
-  Meteor.startup(function () {
-    // code to run on server at startup
+  Meteor.publish('todos', function () {
+    return Todos.find({
+      $or: [
+        { private: {$ne: true} },
+        { owner: this.userId }
+      ]
+    });
   });
 }
